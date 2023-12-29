2e4a64ae0e964e190556a7b760522dd05afd5352
RithanyaG <111141560+RithanyaG@users.noreply.github.com>
2023-12-29 19:23:04 +0530

    Add files via upload

---
+++ pgm2.cpp
@@ -0,0 +1,15 @@
+/*
+	program 2
+	11.12.23
+	expression- c=(a+b)*(a+b)
+*/
+#include <stdio.h>
+main()
+{
+	int a,b,c;
+	a=10;
+	b=10;
+	c=(a+b)*(a+b);
+	printf("%d",c);
+}
+
---
+++ pgm3.cpp
@@ -0,0 +1,17 @@
+/*
+	program 3
+	11.12.23
+	expression- c=(a+b)/(a-b)
+*/
+
+#include <stdio.h>
+main()
+{
+	int a,b;
+	float c;
+	a=10;
+	b=5;
+	c=(a+b)/(a-b);
+	printf("%f",c);
+}
+
