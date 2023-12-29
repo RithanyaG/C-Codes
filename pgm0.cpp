73ae8672d3dd25e9c53710726ece847093a2dba1
RithanyaG <111141560+RithanyaG@users.noreply.github.com>
2023-12-29 18:08:09 +0530

    Add files via upload

---
+++ pgm0.cpp
@@ -0,0 +1,16 @@
+/*
+	Program 0
+	11.12.23
+	Sum of two integer nummbers
+*/
+
+#include<stdio.h>
+main()
+{
+	int a,b,c;
+	a=10;
+	b=20;
+	c=a+b;
+	printf("%d",c);
+}
+
---
+++ pgm1.cpp
@@ -0,0 +1,15 @@
+/*
+	program 1
+	11.12.23
+	product of two reAL NUMBERS
+*/
+#include <stdio.h>
+main()
+{
+	float a,b,c;
+	a=1.0;
+	b=2.0;
+	c=a*b;
+	printf("%f",c);
+}
+
